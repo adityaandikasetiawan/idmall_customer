361893e9c23ebe8051b6b79a4aec1332378180c4
adityaandikasetiawan <adityaandikasetiawan@gmail.com>
2024-04-02 14:03:12 +0700

    'notification'

--- lib/widget/notificationpage.dart
+++ lib/widget/notificationpage.dart
@@ -1,4 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:idmall/models/customer_list..dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:idmall/pages/customer_status.dart';
+import 'package:idmall/config/config.dart' as config;
 
 void main() {
   runApp(MaterialApp(
@@ -20,8 +26,8 @@ class NotificationsPage extends StatelessWidget {
           centerTitle: true,
           bottom: TabBar(
             tabs: [
-              Tab(text: 'Promosi'),
-              Tab(text: 'Status Bar'),
+              Tab(text: 'Informasi'), // Ganti "Promosi" menjadi "Informasi"
+              Tab(text: 'Histori'),
             ],
             indicatorColor:
                 Colors.orange, // Warna latar belakang tab saat aktif
@@ -30,8 +36,8 @@ class NotificationsPage extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            PromotionsPage(),
-            StatusBarPage(),
+            InformationPage(),
+            HistoryList(),
           ],
         ),
       ),
@@ -39,160 +45,225 @@ class NotificationsPage extends StatelessWidget {
   }
 }
 
-class PromotionsPage extends StatelessWidget {
+class InformationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         children: [
-          _buildPromotionCard(
+          _buildInformationCard(
             context,
-            'images/promo1.png',
-            'Promosi 1',
-            'Deskripsi Promosi 1',
+            'Informasi Tagihan',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
           ),
-          _buildPromotionCard(
+          _buildInformationCard(
             context,
-            'images/promo1.png',
-            'Promosi 4',
-            'Deskripsi Promosi 4',
+            'Pemberitahauan Penting',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
           ),
-          _buildPromotionCard(
+          _buildInformationCard(
             context,
-            'images/promo2.png',
-            'Puasa tuh nahan lapar & haus, internetannya jangan ditahan!',
-            'Internet idPlay unlimited bebas kuota lagi promo, nih. Cocok buat kaum mendang-mending. Cus, cek promonya!',
+            'Peringatan Pembayaran',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          ),
+          _buildInformationCard(
+            context,
+            'Promosi',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
           ),
         ],
       ),
     );
   }
 
-  Widget _buildPromotionCard(
+  Widget _buildInformationCard(
     BuildContext context,
-    String imagePath,
     String title,
     String description,
   ) {
-    return Column(
-      children: [
-        Card(
-          margin: EdgeInsets.all(0),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.zero,
-          ),
-          child: Container(
-            height: 200,
-            width: 380,
-            child: ClipRRect(
-              borderRadius: BorderRadius.zero,
-              child: Image.asset(
-                imagePath,
-                fit: BoxFit.cover,
-              ),
-            ),
+    return Card(
+      color: Colors.white,
+      child: ListTile(
+        title: Text(
+          title,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).primaryColor,
           ),
         ),
-        Divider(
-          color: Colors.white,
-          thickness: 0.5,
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 5),
+            Text(description),
+          ],
         ),
-        ListTile(
-          title: Text(
-            title,
-            style: TextStyle(
-              color: const Color.fromARGB(
-                  255, 0, 0, 0), // Warna teks menjadi orange
-            ),
+        trailing: Text(
+          DateFormat('dd MMMM yyyy').format(DateTime.now()), // Tanggal update sekarang
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 12,
           ),
-          subtitle: Text(description),
         ),
-      ],
+      ),
     );
   }
 }
 
-class StatusBarPage extends StatefulWidget {
+class HistoryList extends StatefulWidget {
+  const HistoryList({Key? key}) : super(key: key);
+
   @override
-  _StatusBarPageState createState() => _StatusBarPageState();
+  State<HistoryList> createState() => _HistoryListState();
 }
 
-class _StatusBarPageState extends State<StatusBarPage> {
-  int _currentStep = 0;
-  bool _stepOneComplete = false;
-  bool _stepTwoComplete = false;
-  bool _stepThreeComplete = false;
+class _HistoryListState extends State<HistoryList> {
+  late DateTime now;
+  String? userID;
+  String? total_active;
+  final oCcy = NumberFormat("#,##0", "en_US");
+  final dateFormatter = DateFormat('yyyy-MM-dd');
+  late Future<SharedPreferences> _prefs;
+  late Dio dio;
+
+  @override
+  void initState() {
+    super.initState();
+    now = DateTime.now();
+    _prefs = SharedPreferences.getInstance();
+    dio = Dio();
+    getNameUser();
+    getAchievementList();
+  }
+
+  Future<void> getNameUser() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    final SharedPreferences? prefs = await _prefs;
+
+    setState(() {
+      userID = prefs?.getString('email');
+    });
+  }
+
+  Future<List<CustomerListAchieve>> getAchievementList() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    dateFormatter.format(now);
+    final prefs = await SharedPreferences.getInstance();
+    final String token = prefs.getString('token') ?? "";
+    List<CustomerListAchieve> list = [];
+
+    final response = await dio.get(
+      "${config.backendBaseUrl}/transaction/history",
+      options: Options(headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $token"
+      }),
+    );
+    print(response.data['data']);
+    if (response.data['status'] == 'success') {
+      var hasil = response.data['data'];
+      for (var ele in hasil) {
+        list.add(CustomerListAchieve.fromJson(ele));
+      }
+      return list;
+    } else {
+      return List.empty();
+    }
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: 20),
-        Theme(
-          data: Theme.of(context).copyWith(),
-          child: Stepper(
-            currentStep: _currentStep,
-            onStepTapped: (int index) {
-              // Tidak melakukan apa-apa ketika langkah di-tap
-            },
-            onStepCancel: _currentStep == 2 ? null : _handleCancel,
-            onStepContinue: _currentStep == 2 ? null : _handleContinue,
-            steps: [
-              Step(
-                title: Text('Step 1'),
-                content: Text('Content for Step 1'),
-                isActive: !_stepOneComplete,
-                state:
-                    _stepOneComplete ? StepState.complete : StepState.indexed,
+    final Future<List<CustomerListAchieve>> postsFuture = getAchievementList();
+    return Scaffold(
+      body: RefreshIndicator(
+        color: Colors.white,
+        backgroundColor: Colors.blue,
+        onRefresh: getAchievementList,
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 25,
+            ),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    // height: 600,
+                    child: FutureBuilder<List<CustomerListAchieve>>(
+                      future: postsFuture,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const CircularProgressIndicator();
+                        } else if (snapshot.hasData) {
+                          final posts = snapshot.data!;
+                          return buildPosts(posts);
+                        } else {
+                          return const Text("No data available");
+                        }
+                      },
+                    ),
+                  ),
+                ],
               ),
-              Step(
-                title: Text('Step 2'),
-                content: Text('Content for Step 2'),
-                isActive: !_stepTwoComplete,
-                state:
-                    _stepTwoComplete ? StepState.complete : StepState.indexed,
-              ),
-              Step(
-                title: Text('Step 3'),
-                content: Text('Content for Step 3'),
-                isActive: !_stepThreeComplete,
-                state:
-                    _stepThreeComplete ? StepState.complete : StepState.indexed,
-              ),
-            ],
+            ),
           ),
         ),
-      ],
+      ),
     );
   }
 
-  void _handleContinue() {
-    setState(() {
-      if (_currentStep == 0) {
-        _stepOneComplete = true;
-      } else if (_currentStep == 1) {
-        _stepTwoComplete = true;
-      } else if (_currentStep == 2) {
-        _stepThreeComplete = true;
-      }
-      if (_currentStep < 2) {
-        _currentStep++;
-      }
-    });
-  }
-
-  void _handleCancel() {
-    setState(() {
-      if (_currentStep > 0) {
-        if (_currentStep == 1) {
-          _stepOneComplete = false;
-        } else if (_currentStep == 2) {
-          _stepTwoComplete = false;
-        } else if (_currentStep == 3) {
-          _stepThreeComplete = false;
-        }
-        _currentStep--;
-      }
-    });
+  // function to display fetched data on screen
+  Widget buildPosts(List<CustomerListAchieve> posts) {
+    // ListView Builder to show data in a list
+    return ListView.builder(
+      shrinkWrap: true,
+      primary: false,
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        final post = posts[index];
+        return Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (builder) => CustomerStatus(
+                            status: post.status,
+                            taskid: post.taskID,
+                          )));
+                },
+                leading: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.account_box_rounded,
+                      size: 40,
+                    ),
+                  ],
+                ),
+                isThreeLine: true,
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Reg. No: " + post.taskID.toString()),
+                    Text("Status: " + post.status.toString()),
+                    Text("Product: " + post.serviceName.toString()),
+                  ],
+                ),
+                subtitle: Text(""),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
