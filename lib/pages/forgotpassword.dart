820cc015a0456dff5a8af591e92853bc7b8172fe
adityaandikasetiawan <adityaandikasetiawan@gmail.com>
2024-04-05 08:57:23 +0700

    shared

--- lib/pages/forgotpassword.dart
+++ lib/pages/forgotpassword.dart
@@ -1,36 +1,38 @@
+import 'package:flutter/material.dart';
 import 'package:idmall/pages/signup.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 
 class ForgotPassword extends StatefulWidget {
-  const ForgotPassword({super.key});
+  const ForgotPassword({Key? key}) : super(key: key);
 
   @override
   State<ForgotPassword> createState() => _ForgotPasswordState();
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
-  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController emailController = TextEditingController();
 
   String email = "";
 
-  final _formkey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
-  resetPassword() async {
+  void resetPassword() async {
     try {
       await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text(
-        "Password Reset Email has been sent !",
-        style: TextStyle(fontSize: 18.0),
-      )));
+        content: Text(
+          "Password Reset Email has been sent !",
+          style: TextStyle(fontSize: 18.0),
+        ),
+      ));
     } on FirebaseAuthException catch (e) {
       if (e.code == "user-not-found") {
         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            content: Text(
-          "No user found with that email",
-          style: TextStyle(fontSize: 18.0),
-        )));
+          content: Text(
+            "No user found with that email",
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ));
       }
     }
   }
@@ -56,29 +58,24 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                           Text(
                             "Lupa Password",
                             style: TextStyle(
-                              color:
-                                  Colors.black, // Ubah warna teks menjadi hitam
-                              fontWeight:
-                                  FontWeight.bold, // Tambahkan gaya teks bold
-                              fontSize:
-                                  20.0, // Sesuaikan ukuran font jika diperlukan
-                              fontFamily:
-                                  'Poppins', // Sesuaikan jenis font jika diperlukan
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                              fontFamily: 'Poppins',
                             ),
                           ),
                           const SizedBox(
-                              height:
-                                  20), // Padding tambahan di bagian bawah judul
+                            height: 20,
+                          ),
                         ],
                       ),
                     ),
                   ),
                   Container(
                     padding: const EdgeInsets.all(0.5),
-                    margin: const EdgeInsets.only(
-                        right: 20.0), // Geser ke kanan sebesar 10px
+                    margin: const EdgeInsets.only(right: 20.0),
                     child: Image.asset(
-                      'images/signup.png', // Ganti dengan path gambar yang sesuai
+                      'images/signup.png',
                       height: 250,
                       width: 250,
                     ),
@@ -90,120 +87,130 @@ class _ForgotPasswordState extends State<ForgotPassword> {
               height: 10.0,
             ),
             Expanded(
-                child: Form(
-  key: _formkey,
-  child: Padding(
-    padding: const EdgeInsets.only(left: 15.0, right: 15),
-    child: ListView(
-      children: [
-        Container(
-          padding: const EdgeInsets.only(left: 10.0),
-          decoration: BoxDecoration(
-            border: Border.all(
-                color: const Color.fromARGB(255, 93, 92, 92),
-                width: 2.0),
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: TextFormField(
-            controller: emailcontroller,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please Enter Email';
-              }
-              return null;
-            },
-            style: const TextStyle(
-                color: Color.fromARGB(255, 93, 92, 92)),
-            decoration: const InputDecoration(
-                hintText: "Masukkan Email",
-                hintStyle: TextStyle(
-                  fontSize: 18.0,
-                  color: Color.fromARGB(255, 93, 92, 92),
+              child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 15.0, right: 15),
+                  child: ListView(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: const Color.fromARGB(255, 93, 92, 92),
+                              width: 2.0),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: TextFormField(
+                          controller: emailController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter an email';
+                            }
+                            if (!isValidEmail(value)) {
+                              return 'Please enter a valid email';
+                            }
+                            return null;
+                          },
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 93, 92, 92),
+                          ),
+                          decoration: const InputDecoration(
+                              hintText: "Enter Email",
+                              hintStyle: TextStyle(
+                                fontSize: 18.0,
+                                color: Color.fromARGB(255, 93, 92, 92),
+                              ),
+                              prefixIcon: Icon(
+                                Icons.person,
+                                color: Color.fromARGB(255, 93, 92, 92),
+                                size: 30.0,
+                              ),
+                              border: InputBorder.none),
+                        ),
+                      ),
+                      const SizedBox(height: 8.0),
+                      const Text(
+                        "We will send you a message to reset your password",
+                        style: TextStyle(
+                            fontSize: 10.0,
+                            color: Color.fromARGB(255, 93, 92, 92)),
+                      ),
+                      const SizedBox(
+                        height: 40.0,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          if (_formKey.currentState!.validate()) {
+                            setState(() {
+                              email = emailController.text;
+                            });
+                            resetPassword();
+                          }
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 140,
+                          height: 55,
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 228, 99, 7),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Text(
+                            "Send Email",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 50.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Don't have an account?",
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                color: Color.fromARGB(255, 93, 92, 92)),
+                          ),
+                          const SizedBox(
+                            width: 5.0,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const SignUp()));
+                            },
+                            child: const Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 228, 99, 7),
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-                prefixIcon: Icon(
-                  Icons.person,
-                  color: Color.fromARGB(255, 93, 92, 92),
-                  size: 30.0,
-                ),
-                border: InputBorder.none),
-          ),
-        ),
-        const SizedBox(height: 8.0), // Jarak satu kali
-        const Text(
-          "Kami Akan Mengirimkan Anda Sebuah Pesan Untuk Mengubah Password Anda",
-          style: TextStyle(
-              fontSize: 10.0, color: Color.fromARGB(255, 93, 92, 92)),
-        ),
-        const SizedBox(
-          height: 40.0,
-        ),
-        GestureDetector(
-          onTap: () {
-            if (_formkey.currentState!.validate()) {
-              setState(() {
-                email = emailcontroller.text;
-              });
-              resetPassword();
-            }
-          },
-          child: Container(
-            alignment: Alignment.center,
-            width: 140,
-            height: 55,
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 228, 99, 7),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: const Text(
-              "Kirimkan Email",
-              style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 50.0,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Belum Memiliki Akun?",
-              style: TextStyle(
-                  fontSize: 18.0, color: Color.fromARGB(255, 93, 92, 92)),
-            ),
-            const SizedBox(
-              width: 5.0,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            const SignUp()));
-              },
-              child: const Text(
-                "Daftar",
-                style: TextStyle(
-                    color: Color.fromARGB(255, 228, 99, 7),
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w500),
               ),
-            )
-          ],
-        )
-      ],
-    ),
-  ),
-)
-),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  bool isValidEmail(String email) {
+    // Simple email validation check
+    return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
   }
 }
